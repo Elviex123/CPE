21e2a21f1f70c808d877fcc6a2347baa48a28470
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-09-04 09:45:22 +0800

    1

--- 1star/0802-9.cpp
+++ 1star/0802-9.cpp
@@ -10,15 +10,15 @@ int main() {
         int count = 0;
         carry = 0;
         while (a != 0 || b != 0) {
-            carry += a % 10 + b % 10; // 計算當前位數的和，加上之前的進位
+            carry += a % 10 + b % 10;
             if (carry >= 10) {
-                carry /= 10; // 如果有進位，則進位變為1
-                count++; // 計數進位次數
+                carry /= 10; 
+                count++; 
             } else {
-                carry = 0; // 如果沒有進位，則進位為0
+                carry = 0; 
             }
-            a /= 10; // 移到下一位
-            b /= 10; // 移到下一位
+            a /= 10; 
+            b /= 10;
         }
 
         if (count == 0)
