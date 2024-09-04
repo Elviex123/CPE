f509ed5679008745b3d34c3b9ec7821bba839aef
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2024-09-04 10:06:10 +0800

    1

--- 1star/16.c014. 10035 – Primary Arithmetic.cpp
+++ 1star/16.c014. 10035 – Primary Arithmetic.cpp
@@ -19,37 +19,37 @@ int main(void) {
     int i, j, temp[130];
     char qu[1005];
     struct word ans[130];
-    bool first_output = false;  // 用於控制多行輸入間的換行
+    bool first_output = false;  
 
-    while (fgets(qu, sizeof(qu), stdin) != NULL) {  // 使用 fgets 代替 gets
+    while (fgets(qu, sizeof(qu), stdin) != NULL) { 
         if (first_output) {
             printf("\n");
         }
         first_output = true;
 
-        // 初始化 temp 陣列
+        
         for (i = 0; i < 130; i++) {
             temp[i] = 0;
         }
 
-        // 計算每個字符的出現次數
+        
         for (i = 0; i < strlen(qu); i++) {
             temp[(int)qu[i]]++;
         }
 
-        // 填充 ans 陣列
+        
         for (i = 32, j = 0; i < 128; i++) {
             if (temp[i]) {
-                ans[j].code = i;  // 修正: 應該是 ans[j].code = i 而不是 1
+                ans[j].code = i;  
                 ans[j].time = temp[i];
                 j++;
             }
         }
 
-        // 排序 ans 陣列
+        
         sort(ans, ans + j, cmp);
 
-        // 輸出結果
+    
         for (i = 0; i < j; i++) {
             printf("%d %d\n", ans[i].code, ans[i].time);
         }
--- 1star/29.e507. 10252 – Common Permutation.cpp
+++ 1star/29.e507. 10252 – Common Permutation.cpp
@@ -6,30 +6,30 @@
 using namespace std;
 int main()
 {
-	string a, b;//輸入用字串 
-	int i, t;//方便用 
-	while (getline(cin, a))//輸入資料 
+	string a, b;
+	int i, t;
+	while (getline(cin, a))
 	{
-		getline(cin, b);//輸入資料
-		string re[1000] = { "" };//記錄用陣列 
-		for (i = 0; i < a.length(); i++)//使用雙重迴圈比對 
+		getline(cin, b);
+		string re[1000] = { "" };
+		for (i = 0; i < a.length(); i++)
 		{
 			for (t = 0; t < b.length(); t++)
 			{
-				if (a[i] == b[t])//找到重複的字 
+				if (a[i] == b[t])
 				{
-					re[i] = a[i];//紀錄 
-					b.erase(t, 1);//擦掉該字避免重複找查 
-					break;//直接結束第二個迴圈， 理由同上 
+					re[i] = a[i];
+					b.erase(t, 1);
+					break;
 				}
 			}
 		}
-		sort(re, re + 1000);//排序 
-		for (i = 0; i < 1000; i++)//輸出 
+		sort(re, re + 1000);
+		for (i = 0; i < 1000; i++)
 		{
 			cout << re[i];
 		}
-		cout << endl;//記得換行 
+		cout << endl;
 	}
 	return 0;
 }
